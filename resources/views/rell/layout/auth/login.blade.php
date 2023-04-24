8586c69f8cf04e0cbe88eb9e66af30c1abd0b69e
Alihan Öztürk <alihanozturk364@gmail.com>
2023-04-24 15:26:31 +0300

    Login blade has been  updated

--- resources/views/rell/layout/auth/login.blade.php
+++ resources/views/rell/layout/auth/login.blade.php
@@ -48,9 +48,9 @@
       }
     </style>
   
-    <div class="container px-4 py-5 px-md-5 text-center text-lg-start my-5">
-      <div class="row gx-lg-5 align-items-center mb-5">
-        <div class="col-lg-6 mb-5 mb-lg-0" style="z-index: 10">
+    <div class="container px-4  text-center text-lg-start" style="height:100vh">
+      <div class="row gx-lg-5 align-items-center" style="padding-top:150px">
+        <div class="col-lg-6 mb-5 mb-lg-0" style="z-index: 10;">
           <h1 class="my-5 display-5 fw-bold ls-tight" style="color: hsl(218, 81%, 95%)">
             {!!__('text.login.title')!!}
           </h1>
