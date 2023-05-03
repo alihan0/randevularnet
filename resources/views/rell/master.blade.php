4a2cba358f50524508560e6fb8d987728c29c513
Alihan Öztürk <alihanozturk364@gmail.com>
2023-05-03 22:43:51 +0300

    menü düzenlenmeye başlandı.

--- resources/views/rell/master.blade.php
+++ resources/views/rell/master.blade.php
@@ -234,8 +234,8 @@
 
             <div class="mt-3">
 
-                <a href="#" class="text-dark fw-medium font-size-16">{{Auth::user()->firstname}}</a>
-                <p class="text-body mt-1 mb-0 font-size-13">UI/UX Designer</p>
+                <a href="#" class="text-dark fw-medium font-size-16">{{Auth::user()->firstname.' '.Auth::user()->lastname}}</a>
+                <p class="text-body mt-1 mb-0 font-size-13">{{Auth::user()->company}}</p>
 
             </div>
         </div>
@@ -244,147 +244,57 @@
         <div id="sidebar-menu">
             <!-- Left Menu Start -->
             <ul class="metismenu list-unstyled" id="side-menu">
-                <li class="menu-title">Menu</li>
+                <li class="menu-title">{{__('text.menu.menu')}}</li>
 
                 <li>
                     <a href="javascript: void(0);" class="waves-effect">
-                        <i class="mdi mdi-airplay"></i><span class="badge rounded-pill bg-info float-end">2</span>
-                        <span>Dashboard</span>
+                        <i class="mdi mdi-airplay"></i>
+                        <span>{{__('text.menu.dashboard')}}</span>
                     </a>
-                    <ul class="sub-menu" aria-expanded="false">
-                        <li><a href="index.html">Dashboard 1</a></li>
-                        <li><a href="index-2.html">Dashboard 2</a></li>
-                    </ul>
                 </li>
+                <li>
+                    <a href="javascript: void(0);" class="waves-effect">
+                        <i class="fas fa-calendar-alt"></i>
+                        <span>{{__('text.menu.calendar')}}</span>
+                    </a>
+                </li>
+
+                
+
+                
+
+                
+
+                
+
+                
+
+                <li class="menu-title">{{__('text.menu.manage')}}</li>
 
                 <li>
                     <a href="javascript: void(0);" class="has-arrow waves-effect">
-                        <i class="mdi mdi-flip-horizontal"></i>
-                        <span>Layouts</span>
-                    </a>
-                    <ul class="sub-menu" aria-expanded="true">
-                        <li><a href="javascript: void(0);" class="has-arrow">Vertical</a>
-                            <ul class="sub-menu" aria-expanded="true">
-                                <li><a href="layouts-compact-sidebar.html">Compact Sidebar</a></li>
-                                <li><a href="layouts-icon-sidebar.html">Icon Sidebar</a></li>
-                                <li><a href="layouts-boxed.html">Boxed Layout</a></li>
-                                <li><a href="layouts-preloader.html">Preloader</a></li>
-                            </ul>
-                        </li>
-
-                        <li><a href="javascript: void(0);" class="has-arrow">Horizontal</a>
-                            <ul class="sub-menu" aria-expanded="true">
-                                <li><a href="layouts-horizontal.html">Horizontal</a></li>
-                                <li><a href="layouts-hori-topbarlight.html">Topbar Light</a></li>
-                                <li><a href="layouts-hori-boxed.html">Boxed Layout</a></li>
-                                <li><a href="layouts-hori-preloader.html">Preloader</a></li>
-                            </ul>
-                        </li>
-                    </ul>
-
-                </li>
-
-                <li>
-                    <a href="calendar.html" class=" waves-effect">
-                        <i class="mdi mdi-calendar-text"></i>
-                        <span>Calendar</span>
-                    </a>
-                </li>
-
-                <li>
-                    <a href="javascript: void(0);" class="has-arrow waves-effect">
-                        <i class="mdi mdi-inbox-full"></i>
-                        <span>Email</span>
-                    </a>
-                    <ul class="sub-menu" aria-expanded="false">
-                        <li><a href="email-inbox.html">Inbox</a></li>
-                        <li><a href="email-read.html">Read Email</a></li>
-                    </ul>
-                </li>
-
-                <li>
-                    <a href="javascript: void(0);" class="has-arrow waves-effect">
-                        <i class="mdi mdi-calendar-check"></i>
-                        <span>Tasks</span>
-                    </a>
-                    <ul class="sub-menu" aria-expanded="false">
-                        <li><a href="tasks-list.html">Task List</a></li>
-                        <li><a href="tasks-kanban.html">Kanban Board</a></li>
-                        <li><a href="tasks-create.html">Create Task</a></li>
-                    </ul>
-                </li>
-
-                <li>
-                    <a href="javascript: void(0);" class="has-arrow waves-effect">
-                        <i class="mdi mdi-account-circle-outline"></i>
-                        <span>Pages</span>
-                    </a>
-                    <ul class="sub-menu" aria-expanded="false">
-                        <li><a href="pages-login.html">Login</a></li>
-                        <li><a href="pages-register.html">Register</a></li>
-                        <li><a href="pages-recoverpw.html">Recover Password</a></li>
-                        <li><a href="pages-lock-screen.html">Lock Screen</a></li>
-                        <li><a href="pages-starter.html">Starter Page</a></li>
-                        <li><a href="pages-invoice.html">Invoice</a></li>
-                        <li><a href="pages-profile.html">Profile</a></li>
-                        <li><a href="pages-maintenance.html">Maintenance</a></li>
-                        <li><a href="pages-comingsoon.html">Coming Soon</a></li>
-                        <li><a href="pages-timeline.html">Timeline</a></li>
-                        <li><a href="pages-faqs.html">FAQs</a></li>
-                        <li><a href="pages-pricing.html">Pricing</a></li>
-                        <li><a href="pages-404.html">Error 404</a></li>
-                        <li><a href="pages-500.html">Error 500</a></li>
-                    </ul>
-                </li>
-
-                <li class="menu-title">Components</li>
-
-                <li>
-                    <a href="javascript: void(0);" class="has-arrow waves-effect">
-                        <i class="mdi mdi-checkbox-multiple-blank-outline"></i>
-                        <span>UI Elements</span>
+                        <i class="fas fa-users"></i>
+                        <span>{{__('text.menu.customers')}}</span>
                     </a>
                     <ul class="sub-menu" aria-expanded="false">
                         <li><a href="ui-alerts.html">Alerts</a></li>
                         <li><a href="ui-buttons.html">Buttons</a></li>
                         <li><a href="ui-cards.html">Cards</a></li>
-                        <li><a href="ui-carousel.html">Carousel</a></li>
-                        <li><a href="ui-dropdowns.html">Dropdowns</a></li>
-                        <li><a href="ui-grid.html">Grid</a></li>
-                        <li><a href="ui-images.html">Images</a></li>
-                        <li><a href="ui-lightbox.html">Lightbox</a></li>
-                        <li><a href="ui-modals.html">Modals</a></li>
-                        <li><a href="ui-rangeslider.html">Range Slider</a></li>
-                        <li><a href="ui-session-timeout.html">Session Timeout</a></li>
-                        <li><a href="ui-progressbars.html">Progress Bars</a></li>
-                        <li><a href="ui-sweet-alert.html">Sweet-Alert</a></li>
-                        <li><a href="ui-tabs-accordions.html">Tabs & Accordions</a></li>
-                        <li><a href="ui-typography.html">Typography</a></li>
-                        <li><a href="ui-video.html">Video</a></li>
-                        <li><a href="ui-general.html">General</a></li>
-                        <li><a href="ui-colors.html">Colors</a></li>
-                        <li><a href="ui-rating.html">Rating</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="javascript: void(0);" class="has-arrow waves-effect">
+                        <i class="fas fa-user-tie"></i>
+                        <span>{{__('text.menu.staff')}}</span>
+                    </a>
+                    <ul class="sub-menu" aria-expanded="false">
+                        <li><a href="ui-alerts.html">Alerts</a></li>
+                        <li><a href="ui-buttons.html">Buttons</a></li>
+                        <li><a href="ui-cards.html">Cards</a></li>
                     </ul>
                 </li>
 
-                <li>
-                    <a href="javascript: void(0);" class="waves-effect">
-                        <i class="mdi mdi-newspaper"></i>
-                        <span class="badge rounded-pill bg-danger float-end">6</span>
-                        <span>Forms</span>
-                    </a>
-                    <ul class="sub-menu" aria-expanded="false">
-                        <li><a href="form-elements.html">Form Elements</a></li>
-                        <li><a href="form-validation.html">Form Validation</a></li>
-                        <li><a href="form-advanced.html">Form Advanced</a></li>
-                        <li><a href="form-editors.html">Form Editors</a></li>
-                        <li><a href="form-uploads.html">Form File Upload</a></li>
-                        <li><a href="form-xeditable.html">Form Xeditable</a></li>
-                        <li><a href="form-repeater.html">Form Repeater</a></li>
-                        <li><a href="form-wizard.html">Form Wizard</a></li>
-                        <li><a href="form-mask.html">Form Mask</a></li>
-                    </ul>
-                </li>
+                
 
                 <li>
                     <a href="javascript: void(0);" class="has-arrow waves-effect">
