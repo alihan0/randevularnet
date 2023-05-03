c3017b0351999706c7eb0983a08a9cf060e31e9f
Alihan Öztürk <alihanozturk364@gmail.com>
2023-05-03 21:17:48 +0300

    Dil switch eklendi.

--- resources/views/rell/master.blade.php
+++ resources/views/rell/master.blade.php
@@ -57,36 +57,27 @@
                             </div>
                         </div>
 
+                        <div class="dropdown d-none d-lg-inline-block ms-1">
+                            <button type="button" class="btn header-item noti-icon waves-effect" data-toggle="fullscreen">
+                                <i class="fas fa-moon"></i>
+                            </button>
+                        </div>
+
                         <div class="dropdown d-none d-sm-inline-block">
                             <button type="button" class="btn header-item waves-effect" data-bs-toggle="dropdown"
                                 aria-haspopup="true" aria-expanded="false">
-                                <img class="" src="/static/assets/images/flags/us.jpg" alt="Header Language" height="16">
+                                <img class="" src="/static/assets/images/flags/{{LaravelLocalization::getCurrentLocale()}}.png" alt="Header Language" height="16">
                             </button>
                             <div class="dropdown-menu dropdown-menu-end">
-
-                                <!-- item-->
-                                <a href="javascript:void(0);" class="dropdown-item notify-item">
-                                    <img src="/static/assets/images/flags/spain.jpg" alt="user-image" class="me-1" height="12"> <span
-                                        class="align-middle">Spanish</span>
-                                </a>
-
-                                <!-- item-->
-                                <a href="javascript:void(0);" class="dropdown-item notify-item">
-                                    <img src="/static/assets/images/flags/germany.jpg" alt="user-image" class="me-1" height="12"> <span
-                                        class="align-middle">German</span>
-                                </a>
-
-                                <!-- item-->
-                                <a href="javascript:void(0);" class="dropdown-item notify-item">
-                                    <img src="/static/assets/images/flags/italy.jpg" alt="user-image" class="me-1" height="12"> <span
-                                        class="align-middle">Italian</span>
-                                </a>
-
-                                <!-- item-->
-                                <a href="javascript:void(0);" class="dropdown-item notify-item">
-                                    <img src="/static/assets/images/flags/russia.jpg" alt="user-image" class="me-1" height="12"> <span
-                                        class="align-middle">Russian</span>
-                                </a>
+                                @foreach(LaravelLocalization::getSupportedLocales() as $localeCode => $properties)
+                                    <a rel="alternate" hreflang="{{$localeCode}}" href="{{LaravelLocalization::getLocalizedURL($localeCode, null, [], true)}}" class="dropdown-item notify-item">
+                                        <img src="/static/assets/images/flags/{{$localeCode}}.png" alt="user-image" class="me-1" height="12">
+                                        <span>
+                                            {{$properties['native']}}
+                                        </span>
+                                    </a>
+                                @endforeach
+                                
                             </div>
                         </div>
 
